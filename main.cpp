720e27e55ade129c9273b9a7f50d8671f6d9d0bf
John Carlson <carlsonjjohn@gmail.com>
2020-03-16 15:43:48 -0800

    v0.5 from 2003/01/31 - created a supercub class that inherits from the sprite class, also handled input cases

--- main.cpp
+++ main.cpp
@@ -9,16 +9,24 @@
 //
 //					At this point the sprite handling has been transfered to the Jsprite class
 //					it is still however mostly hard coded :( so it currently won't do more than
-//					one sprite.
+//					one sprite.  Also made a pile of macros to define and implement basic attributes
 //
 //					changes 1-31-2003
 //
+//					ver.0.3
 //					Jsprite.initSprite() now takes a ptr to the array representing the bmp
-//					...
+//
+//					ver.0.4
 //					Can now make different sprites based.  I ditched the idea of having one big
 //					static array of 128 sprites in the sprite class because the compiler balked
 //					at it.  So each instance has a u8 that corresponds with its sprite data.
 //					initSprite now also takes a screen x&y position
+//
+//					ver.0.5
+//					Added a few input functions to mygba.h to check for specific key combos and 
+//					others that check for only a single button being pushed.  Also mucked with the
+//					background mode to allow text display.  The background, previously bitmap, is
+//					now white with dots...
 
 
 
@@ -53,20 +61,20 @@ int main(void)
 
 	map_fragment_info_ptr  bg_pic; //map pointer
 
-	ham_SetBgMode(1);	//set background mode to one
+	ham_SetBgMode(0);	//set background mode to one
 
     ham_LoadBGPal256((void*)&spr_Palette);
 	
-	
+	ham_InitText(0);
 	//now load the supercubs color palette
 	ham_LoadObjPal((void*)&object_Palette,256);
 
-	ham_bg[0].ti = ham_InitTileSet((void*)&sprite_Tiles,		//ptr to source
+	ham_bg[1].ti = ham_InitTileSet((void*)&sprite_Tiles,		//ptr to source
 								   SIZEOF_16BIT(sprite_Tiles),	//number of 16bit chunks
 								   1,							//color/palette mode
 								   1);							//offset or not...
 
-	ham_bg[0].mi = ham_InitMapEmptySet(3,				//map size				
+	ham_bg[1].mi = ham_InitMapEmptySet(3,				//map size				
 									   0);				//rotatable (0 = no)
 
 	bg_pic = ham_InitMapFragment((void*)&sprite_Map,	//ptr to source
@@ -85,7 +93,7 @@ int main(void)
 
 
 	//Display the background
-	ham_InitBg(0,		//background number
+	ham_InitBg(1,		//background number
 			   1,		//turn bg on/off (1 = on)
 			   0,		//set priority (0-high,3-low)
 			   0);		//turn mosaic on/off
@@ -93,8 +101,8 @@ int main(void)
 	//initialize our supercub sprite
 	plane.initSprite(0,										//sprite index number 0 - 127
 					 (unsigned char*)&supercub_anim_Bitmap,	//pointer to bmp array
-					 110,
-					 50
+					 110,									//xscreen coord
+					 50										//yscreen coord
 					);
 
 	otherplane.initSprite(1,(unsigned char*)&supercub_anim_Bitmap,10,10);
@@ -127,6 +135,7 @@ int main(void)
 	//infinite loop so program doesn't end
     while(1)
     {
+		
     }    
 }//end main
 
@@ -151,28 +160,72 @@ void vblFunc()
 	
 void query_keys()
 {
-	if(F_CTRLINPUT_RIGHT_PRESSED && plane.getTextureIndex() > 0)
-	{
-		//rotate the plane back to the left
+	//all references to axis assume positive z comes directly out of the screen,
+	// positive x extends to the right and positive y extend up the screen
+	
+	if (F_CTRLINPUT_UP_ONLY_PRESSED){
+		//rotate plane around x axis
+		//rotate elevators down
+		// make the plane dive
+		ham_DrawText(1,18,"up only");
+	}
+	else if (F_CTRLINPUT_DOWN_ONLY_PRESSED) {
+		//rotate plane around x axis
+		//rotate elevators up
+		// make the plane climb
+		ham_DrawText(1,18,"down only");
+	}
+	else if (F_CTRLINPUT_LEFT_ONLY_PRESSED) {
+		//rotate the plane around the z axis
+		//rotate the rudder left
+		// make the plane turn left
+
 		plane.setTextureIndex(plane.getTextureIndex()-1);
 		otherplane.setTextureIndex(plane.getTextureIndex()-1);
 		in_rot = 1;
+		ham_DrawText(1,18,"left only");
 
 	}
-
-	if(F_CTRLINPUT_LEFT_PRESSED && plane.getTextureIndex() < 15)
-	{
-		//rotate the plane to the right
+	else if (F_CTRLINPUT_RIGHT_ONLY_PRESSED) {
+		//rotate the plane around the z axis
+		//rotate the rudder right
+		// make the plane turn right
 		plane.setTextureIndex(plane.getTextureIndex()+1);
 		otherplane.setTextureIndex(plane.getTextureIndex()+1);
 		in_rot = 1;
+		ham_DrawText(1,18,"right only   ");
+	}
+	else if (F_CTRLINPUT_A_ONLY_PRESSED) {
+		// varies with mission
+		// example: drop supplies
+		ham_DrawText(1,18,"a only       ");
+	}
+	else if (F_CTRLINPUT_B_ONLY_PRESSED) {
+		// nothing...
+		ham_DrawText(1,18,"b only       ");
+	}
+	else if (F_CTRLINPUT_R_ONLY_PRESSED) {
+		//rotate the plane around the y axis
+		//rotate the ailerons
+		// make the plane roll right
+		ham_DrawText(1,18,"r only       ");
+	}
+	else if (F_CTRLINPUT_L_ONLY_PRESSED) {
+		//rotate the plane around the y axis
+		//rotate the ailerons
+		// make the plane roll left
+		ham_DrawText(1,18,"l only       ");
+	}
+	else if (F_CTRLINPUT_B_AND_UP_PRESSED) {
+		//increase plane speed
+		ham_DrawText(1,18,"b and up     ");
+	}
+	else if (F_CTRLINPUT_B_AND_DOWN_PRESSED) {
+		//decrease plane speed
+        ham_DrawText(1,18,"b and down   ");
 	}
 
 }//end query_keys
-
-
-
-
 
 
 
